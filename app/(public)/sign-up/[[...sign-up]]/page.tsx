69e8dc73cf7c0d65cd5c2087b2e01021fded6d3c
Lucas GEGOT <lucas.gegot@partnre.com>
2025-02-03 13:53:59 +0100

    🚧 Remove clerk/elements to make api call

--- app/(public)/sign-up/[[...sign-up]]/page.tsx
+++ app/(public)/sign-up/[[...sign-up]]/page.tsx
@@ -1,93 +1,160 @@
 "use client";
-
-import * as Clerk from "@clerk/elements/common";
-import * as SignUp from "@clerk/elements/sign-up";
-import { AppleLogo, GoogleLogo, MicrosoftLogo } from "@/components/icons/index";
+import * as React from "react";
+import { useSignUp } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import { AnimatedBG } from "@/components/ui/AnimatedBG/AnimatedBG";
-import Link from "next/link";
+import { Input } from "@/components/ui/input";
 
 export default function SignUpPage() {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [emailAddress, setEmailAddress] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [verifying, setVerifying] = React.useState(false);
+  const [code, setCode] = React.useState("");
+  const router = useRouter();
+
+  // Handle submission of the sign-up form
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!isLoaded) return;
+
+    // Start the sign-up process using the email and password provided
+    try {
+      await signUp.create({
+        emailAddress,
+        password,
+      });
+
+      // Send the user an email with the verification code
+      await signUp.prepareEmailAddressVerification({
+        strategy: "email_code",
+      });
+
+      // Set 'verifying' true to display second form
+      // and capture the OTP code
+      setVerifying(true);
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  // Handle the submission of the verification form
+  const handleVerify = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!isLoaded) return;
+
+    try {
+      // Use the code the user provided to attempt verification
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      // If verification was completed, set the session to active
+      // and redirect the user
+      if (signUpAttempt.status === "complete") {
+        await setActive({ session: signUpAttempt.createdSessionId });
+        router.push("/");
+      } else {
+        // If the status is not complete, check why. User may need to
+        // complete further steps.
+        console.error(JSON.stringify(signUpAttempt, null, 2));
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error("Error:", JSON.stringify(err, null, 2));
+    }
+  };
+
+  // Display the verification form to capture the OTP code
+  if (verifying) {
+    return (
+      <>
+        <h1>Verify your email</h1>
+        <form onSubmit={handleVerify}>
+          <label id="code">
+            Enter your verification code
+            <input
+              value={code}
+              id="code"
+              name="code"
+              onChange={(e) => setCode(e.target.value)}
+            />
+          </label>
+          <button type="submit">Verify</button>
+        </form>
+      </>
+    );
+  }
+
+  // Display the initial sign-up form to capture the email and password
   return (
-    <div>
+    <>
       <AnimatedBG />
       <div className="flex justify-center items-center h-screen">
-        <div className="w-4/5 md:w-1/2 flex flex-col items-center justify-center border bg-white align-middle rounded-lg shadow-lg pb-24 pt-12 px-6 relative">
-          <SignUp.Root>
-            <SignUp.Step name="start">
-              <h1 className="flex flex-col items-center justify-center text-2xl pb-8">
-                <span>Inscription</span>
-                <span>Cet Extra!</span>
-              </h1>
-              <div className="flex flex-col gap-4">
-                <Clerk.Field
-                  name="emailAddress"
-                  className="flex flex-col items-center gap-1"
-                >
-                  <Clerk.Label>Email:</Clerk.Label>
-                  <Clerk.Input className="border rounded-md pl-2 w-3/4" />
-                  <Clerk.FieldError className="text-xs italic" />
-                </Clerk.Field>
-                <Clerk.Field
-                  name="password"
-                  className="flex flex-col items-center gap-1"
-                >
-                  <Clerk.Label>Mot de passe</Clerk.Label>
-                  <Clerk.Input className="border rounded-md  pl-2 w-3/4" />
-                  <Clerk.FieldError className="text-xs italic" />
-                </Clerk.Field>
+        <div className="w-4/5 md:w-1/2 flex flex-col items-center justify-center border bg-white align-middle rounded-lg shadow-lg pb-24 pt-12 px-6 relative md:grid md:grid-cols-2">
+          <div className=""></div>
+          <div className="">
+            <h1 className="flex flex-col items-center justify-center pb-8">
+              <span className="text-5xl">Cet Extra!</span>
+              <span className=" text-2xl">Inscription</span>
+            </h1>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 pl-5">
+              <div className="flex flex-col gap-1">
+                <label htmlFor="email">Entrez votre adresse email:</label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={emailAddress}
+                  onChange={(e) => setEmailAddress(e.target.value)}
+                  className="w-3/4"
+                />
               </div>
-              <SignUp.Captcha />
+              <div className="flex flex-col gap-1">
+                <label htmlFor="password">Entrez votre mot de passe:</label>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Mot de passe"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-3/4"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label htmlFor="password">Confirmez votre mot de passe:</label>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Confirmation mot de passe"
+                  value={password}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  className="w-3/4"
+                />
+              </div>
+              {/* CAPTCHA Widget */}
+              <div id="clerk-captcha"></div>
               <div className="flex justify-center">
-                <SignUp.Action
-                  submit
+                <button
+                  type="submit"
                   className="border rounded-lg bg-blue-500 text-white py-2 px-4 my-4 hover:bg-blue-600"
                 >
-                  Inscription
-                </SignUp.Action>
+                  Continue
+                </button>
               </div>
-              <div className="">
-                <p className="text-lg">Ou bien connectez-vous via:</p>
-                <div className="flex justify-center items-center gap-x-9 py-4">
-                  <Clerk.Connection name="google">
-                    <div className="border-black border-2 rounded-full p-3 flex items-center justify-center">
-                      <GoogleLogo />
-                    </div>
-                  </Clerk.Connection>
-
-                  <Clerk.Connection name="apple">
-                    <div className="border-black border-2  rounded-full p-3 flex items-center justify-center">
-                      <AppleLogo />
-                    </div>
-                  </Clerk.Connection>
-                  <Clerk.Connection name="microsoft">
-                    <div className="border-black border-2 rounded-full p-3 flex items-center justify-center">
-                      <MicrosoftLogo />
-                    </div>
-                  </Clerk.Connection>
-                </div>
-              </div>
-            </SignUp.Step>
-
-            <SignUp.Step name="verifications">
-              <SignUp.Strategy name="email_code">
-                <h1>Vérifiez vos emails</h1>
-                <Clerk.Field name="code">
-                  <Clerk.Label>Email code:</Clerk.Label>
-                  <Clerk.Input className="border rounded-md pl-2" />
-                  <Clerk.FieldError className="text-xs italic" />
-                </Clerk.Field>
-                <SignUp.Action submit>Continue</SignUp.Action>
-              </SignUp.Strategy>
-            </SignUp.Step>
-          </SignUp.Root>
-
-          <div className="absolute bottom-4">
-            <Link href="/sign-in" className="text-xs hover:underline">
-              Déjà inscrit? Connecte toi ici!
-            </Link>
+            </form>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
