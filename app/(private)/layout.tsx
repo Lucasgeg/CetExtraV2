54f3648a098f1df7288d7297c3998ab2c09a8ca2
LucasG <sulac@live.fr>
2025-07-16 20:40:44 +0200

    💄 Better display

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -3,7 +3,6 @@ import { CetExtraLogo } from "@/components/icons/CetExtraLogo";
 import { Cog6ToothIcon } from "@heroicons/react/24/outline";
 import ErrorBoundary from "@/components/ErrorBoundary/ErrorBoundary";
 import Link from "next/link";
-
 export default function RootLayout({
   children
 }: Readonly<{
@@ -11,8 +10,9 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <div className="mx-auto flex h-full w-full flex-col px-6 antialiased">
-        <div className="relative flex items-center">
+      <div className="mx-auto flex h-full w-full flex-col sm:px-6">
+        {/* Header fixe */}
+        <header className="relative flex items-center py-2">
           <Link href={"/company"}>
             <CetExtraLogo className="h-16 lg:h-24" />
           </Link>
@@ -27,13 +27,20 @@ export default function RootLayout({
               <UserButton />
             </div>
           </div>
-        </div>
-        <div className="my-auto flex h-[80vh] flex-col overflow-y-auto bg-main-gradient p-6">
-          <div className="pointer-events-none absolute left-1/4 top-1/4 aspect-square w-1/4 max-w-[384px] animate-pulse rounded-full bg-blue-500/10 blur-3xl"></div>
-          <div className="pointer-events-none absolute bottom-1/4 right-1/4 aspect-square w-1/5 max-w-[320px] animate-pulse rounded-full bg-purple-500/10 blur-3xl delay-1000"></div>
-          <div className="pointer-events-none absolute right-1/3 top-1/2 aspect-square w-1/6 max-w-[256px] animate-pulse rounded-full bg-cyan-500/10 blur-3xl delay-500"></div>
-          <ErrorBoundary>{children}</ErrorBoundary>
-        </div>
+        </header>
+
+        {/* Contenu principal avec overflow */}
+        <main className="my-auto max-h-[80vh] flex-1 overflow-hidden bg-main-gradient px-2 py-6 sm:px-6">
+          <div className="relative h-full overflow-y-auto">
+            {/* Éléments d'animation en arrière-plan */}
+            <div className="pointer-events-none absolute left-1/4 top-1/4 aspect-square w-1/4 max-w-[384px] animate-pulse rounded-full bg-blue-500/10 blur-3xl"></div>
+            <div className="pointer-events-none absolute bottom-1/4 right-1/4 aspect-square w-1/5 max-w-[320px] animate-pulse rounded-full bg-purple-500/10 blur-3xl delay-1000"></div>
+            <div className="pointer-events-none absolute right-1/3 top-1/2 aspect-square w-1/6 max-w-[256px] animate-pulse rounded-full bg-cyan-500/10 blur-3xl delay-500"></div>
+
+            {/* Contenu de la page */}
+            <ErrorBoundary>{children}</ErrorBoundary>
+          </div>
+        </main>
       </div>
     </ClerkProvider>
   );
