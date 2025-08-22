0f5fb335198aa78518695e622dc84a8940337a85
LucasG <sulac@live.fr>
2025-08-22 15:55:57 +0200

    🗃️ Update company table

---
+++ prisma/migrations/20250822135444_remove_unnecessary_default_value/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Company" ALTER COLUMN "contactFirstName" DROP DEFAULT,
+ALTER COLUMN "contactLastName" DROP DEFAULT,
+ALTER COLUMN "businessSector" DROP DEFAULT,
+ALTER COLUMN "collectiveAgreement" DROP DEFAULT,
+ALTER COLUMN "headOfficeAddress" DROP DEFAULT,
+ALTER COLUMN "legalRepresentativeFunction" DROP DEFAULT;
